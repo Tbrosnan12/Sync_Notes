7c021a29aab9639e504c660362b68135c7f2b2e8
Tbrosnan12 <tbrosnan@tcd.ie>
2024-08-24 01:10:40 +0100

    added update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git_username=Tbrosnan12
-
-
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo "Warning: you are not in a git repo directory"
 else
@@ -13,7 +10,8 @@ else
 
     dir=$(pwd)
     repo_name=$(echo "${dir}" | awk -F'/' '{print $NF}')
-
+    git_username=$(git config user.name)
+    
     if [ "$#" -gt 1 ]; then
         for i in $@; do
             commit_message+="${i} "     #check if there is a commit message 
@@ -22,19 +20,15 @@ else
 
     #Fetch origin to compare 
     git fetch origin --quiet
-    git status | grep behind > /dev/null 2>&1      # pull from origin if behind
-    if [ $? = 0 ]; then
+    if git status | grep behind > /dev/null 2>&1; then               # pull from origin if behind
         echo "repo ${repo_name} is behind origin, pulling..."  
         git pull origin main --quiet
         done=true
         up_to_date=false
     fi
                
-                                                   # do nothing if nothing to add 
-    git status | grep "nothing to commit" > /dev/null 2>&1
-    if [ $? = 0 ]; then
-        git status | grep "up to date with" > /dev/null 2>&1   #May be nothing to add, but still be a merger issue 
-        if [ $? = 0 ]; then
+    if git status | grep "nothing to commit" > /dev/null 2>&1; then     # do nothing if nothing to add 
+        if git status | grep "up to date with" > /dev/null 2>&1; then    #May be nothing to add, but still be a merger issue 
             done=true
         else
             merger_issue=true
@@ -50,8 +44,7 @@ else
         else
             git commit -m "${commit_message}" > /dev/null  #add optional commit message 
         fi
-        git status | grep ahead > /dev/null
-        if [ $? = 0 ]; then
+        if git status | grep ahead > /dev/null 2>&1; then
             echo "repo ${repo_name} is ahead of origin, pushing..."    # push to origin if ahead 
             git remote set-url origin git@github.com:${git_username}/${repo_name}.git
             git push origin main --quiet
@@ -60,15 +53,15 @@ else
         done=true
     fi
 
-
+    #if nothing has been "done" at this stage there is a merger issue
     if [ "$done" = "false" ]; then
         merger_issue=true
         up_to_date=false
     fi   
 
-
+    #echo what has happened :)
     if [ "${up_to_date}" = "true" ]; then
-        echo "no updates needed :)"      
+        echo "No updates needed :)"      
     elif [ "$merger_issue" = true ]; then
         echo "Uh oh seems there is a merger issue"         
     else
