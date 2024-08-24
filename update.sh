4db2c5812002fe3aacd432b14a56ba56b0a7c3fb
Tbrosnan12 <tbrosnan@tcd.ie>
2024-08-24 13:12:49 +0100

    fixed pushing and pulling structure

--- update.sh
+++ update.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+function pull() {
+    local repo_name=$1 
+    echo "repo ${repo_name} is behind origin, pulling..."  
+    git pull origin main --quiet
+}
+
+function commit() {
+    git add --all > /dev/null
+    if [ -z "$commit_message" ]; then
+        git commit -m "auto-sync" > /dev/null
+    else
+        git commit -m "${commit_message}" > /dev/null  #add optional commit message 
+    fi
+}
+
+function push() {
+    local repo_name=$1
+    echo "repo ${repo_name} is ahead of origin, pushing..."     
+    git remote set-url origin git@github.com:${git_username}/${repo_name}.git
+    git push origin main --quiet
+}
+
+
+
 if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
     echo "Warning: you are not in a git repo directory"
 else
-    up_to_date=true
+    up_to_date=false
     merger_issue=false
     done=false
     commit_message=""
 
-    dir=$(git rev-parse --show-toplevel)
+    dir=$(git rev-parse --show-toplevel)    #directory of top directory in the git repo tree
     repo_name=$(echo "${dir}" | awk -F '/' '{print $NF}')
     git_username=$(git config user.name)
     
@@ -21,44 +45,28 @@ else
 
     #Fetch origin to compare 
     git fetch origin --quiet
-    if git status | grep behind > /dev/null 2>&1; then  # pull from origin if behind
-        echo "repo ${repo_name} is behind origin, pulling..."  
-        git pull origin main --quiet
-        done=true
-        up_to_date=false
-    fi
-               
+
     if git status | grep "nothing to commit" > /dev/null 2>&1; then     # do nothing if nothing to add 
         if git status | grep "up to date with" > /dev/null 2>&1; then    #May be nothing to add, but still be a merger issue 
-            done=true
+            up_to_date=true
+        elif git status | grep behind > /dev/null 2>&1; then  # pull from origin if behind
+            pull "${repo_name}"  
         else
             merger_issue=true
-            up_to_date=false
+        fi
+    else
+        commit
+        if git status | grep "diverged" > /dev/null 2>&1; then
+            merger_issue=true
+        elif git status | grep behind > /dev/null 2>&1; then  # pull from origin if behind
+            pull "${repo_name}"
+        elif git status | grep "up to date with" > /dev/null 2>&1; then    #May be nothing to add, but still be a merger issue 
+            up_to_date=true
+        elif git status | grep ahead > /dev/null 2>&1; then   # push to origin if ahead
+            push "${repo_name}"
         fi
     fi
 
-
-    if [ "$done" = "false" ]; then  #If stuff to add check if ahead 
-        git add --all > /dev/null
-        if [ -z "$commit_message" ]; then
-            git commit -m "auto-sync" > /dev/null
-        else
-            git commit -m "${commit_message}" > /dev/null  #add optional commit message 
-        fi
-        if git status | grep ahead > /dev/null 2>&1; then   # push to origin if ahead
-            echo "repo ${repo_name} is ahead of origin, pushing..."     
-            git remote set-url origin git@github.com:${git_username}/${repo_name}.git
-            git push origin main --quiet
-        fi
-        up_to_date=false
-        done=true
-    fi
-
-    #if nothing has been "done" at this stage there is a merger issue
-    if [ "$done" = "false" ]; then
-        merger_issue=true
-        up_to_date=false
-    fi   
 
     #echo what has happened :)
     if [ "${up_to_date}" = "true" ]; then
