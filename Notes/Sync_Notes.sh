a45bdae1e1e443ef17d89871d7f81da97304f1c2
Tbrosnan12 <tbrosnan@tcd.ie>
2024-08-24 01:21:18 +0100

    move stuff around

--- Notes/Sync_Notes.sh
+++ Notes/Sync_Notes.sh
@@ -12,19 +12,16 @@ for dir in "${Note_directories[@]}"; do
     done=false
 
     #Fetch origin to compare 
-    git fetch origin --quiet
-    git status | grep behind > /dev/null 2>&1      # pull from origin if behind
-    if [ $? = 0 ]; then
+    git fetch origin --quiet     
+    if git status | grep behind > /dev/null 2>&1; then  # pull from origin if behind
         echo "repo ${repo_name} is behind origin, pulling..."  
         git pull origin main --quiet
         done=true
         up_to_date=false
     fi
-                                                   # do nothing if nothing to add 
-    git status | grep "nothing to commit" > /dev/null 2>&1
-    if [ $? = 0 ]; then
-        git status | grep "up to date with" > /dev/null 2>&1   #May be nothing to add, but still be a merger issue 
-        if [ $? = 0 ]; then
+                                                   
+    if git status | grep "nothing to commit" > /dev/null 2>&1; then    # do nothing if nothing to add  
+        if git status | grep "up to date with" > /dev/null 2>&1; then  #May be nothing to add, but still be a merger issue 
             done=true
         else
             merger_issue=true
@@ -35,10 +32,9 @@ for dir in "${Note_directories[@]}"; do
     if [ "$done" = "false" ]; then                #If stuff to add check if ahead 
         git add --all > /dev/null
         git commit -m "auto-sync" > /dev/null
-        git status | grep ahead > /dev/null
-        if [ $? = 0 ]; then
+        if git status | grep ahead > /dev/null 2>&1; then
             echo "repo ${repo_name} is ahead of origin, pushing..."    # push to origin if ahead 
-            git remote set-url origin git@github.com:Tbrosnan12/${repo_name}.git
+            git remote set-url origin git@github.com:${git_username}/${repo_name}.git
             git push origin main --quiet
         fi
         up_to_date=false
@@ -51,9 +47,9 @@ for dir in "${Note_directories[@]}"; do
 done
 
 if [ "${up_to_date}" = "true" ]; then
-    echo "All up to date :)"      
+    echo "No updates needed :)"      
 elif [ "$merger_issue" = true ]; then
-    echo "Uh oh seems there is a merger issue"         
+    echo "Uh oh seems there is a merger issue :("         
 else
     echo "All done!"
 fi
